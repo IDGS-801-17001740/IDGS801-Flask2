4e00427bb02ddfe2b62c75c442fc00e959d829a1
IDGS-801-17001740 <17001740@alumnos.utleon.edu.mx>
2023-02-20 22:34:38 -0600

    20/02/2023

---
+++ archivos.py
@@ -0,0 +1,7 @@
+file = open('alumnos.txt','r')
+nombres = file.readlines()
+print(nombres)
+file.close()
+
+for item in nombres:
+    print(item,end = '')
---
+++ forms.py
@@ -0,0 +1,14 @@
+from wtforms import Form
+from wtforms import StringField, SubmitField, FieldList, FormField, SelectField
+from flask_wtf import FlaskForm
+
+from wtforms.fields import EmailField, TextAreaField, RadioField, PasswordField
+from wtforms import validators
+
+class UserForm(Form):
+    matricula = StringField('Matricula',[validators.DataRequired(message = 'El campo es requerido'),
+                                         validators.length(min = 4, max = 10, message = 'long de campo 4min and 10 max')])
+    nombre = StringField('Nombre',[validators.DataRequired(message = 'El campo es requerido')])
+    apaterno = StringField('Apaterno')
+    amaterno = StringField('Amaterno')
+    email = EmailField('Email')

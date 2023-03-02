48320c9d7859bb6b26b7c90beeba34cb1f378fe9
IDGS-801-17001740 <17001740@alumnos.utleon.edu.mx>
2023-03-02 11:10:24 -0600

    20/03/2023

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
-from flask import Flask, request, render_template, redirect
+from flask import Flask, request, render_template, redirect, make_response, flash
+from flask_wtf import CSRFProtect
 
 import forms
-from Calculadora import Calculadora
+from Operaciones import Calculadora, Traductor
 
 app = Flask(__name__)
+#app.config['SECRET_KEY'] = 'tu_clave_secreta_aqui'
+#csrf = CSRFProtect(app)
 
-@app.route('/')
-def mainn():
-    return redirect('/cajas_dinamica')
 
 @app.route('/alumnos', methods=['GET', 'POST'])
 def alumnos():
@@ -19,7 +19,7 @@ def alumnos():
         Mat = registrar_Alumno.matricula.data
         Nom = registrar_Alumno.nombre.data
     
-    return render_template('alumnos.html', form = registrar_Alumno, mat=Mat, nom = Nom)
+    return render_template('alumnos.html', form = registrar_Alumno, mat=Mat, nom = Nom, name="Alumnos")
 
 @app.route('/cajas_dinamica', methods=['GET', 'POST'])
 def method_name(): 
@@ -46,5 +46,42 @@ def calcular():
                            nMayor = Calculadora.num_Mayor(Numeros),
                            name = "Resultado de " + str(len(Numeros)) + " números")
 
+@app.route('/cookie', methods=['GET', 'POST'])
+def cookies():
+#    return 'HOLA'
+    reg_user = forms.LoginForm(request.form)
+    response = make_response(render_template('cookie.html', name = 'Cookie', form = reg_user))
+    
+    if request.method == 'POST' and reg_user.validate():
+        user = reg_user.username.data
+        passw = reg_user.password.data
+        datos = user + '@' + passw
+        success_message = 'Bienvenido {}'.format(user)
+        response.set_cookie('datos_user', datos)
+        flash(success_message)
+    
+    return response
+
+@app.route('/traductor', methods=['GET', 'POST'])
+def traductor():
+    reg_palabra = forms.Languages(request.form)
+    result = ''
+    word = ''
+    
+    if request.method == 'POST':
+        status = request.form.get('status')
+        if status == 'registrar' and reg_palabra.validate():
+            span = reg_palabra.spanish.data
+            en = reg_palabra.english.data
+            Traductor.guardar(span, en)
+        elif status == 'buscar':
+            word = request.form.get('word')
+            lan = request.form.get('select')
+            result = "Traducción: {}".format(Traductor.buscar_Palabra(word, lan))
+            
+    
+    return render_template('traductor.html', name='Traductor', form=reg_palabra, result = result, search = word)
+
 if __name__ == "__main__":
+    #csrf.init_app(app)
     app.run(debug = True, port=3000)
--- archivos.py
+++ archivos.py
@@ -1,7 +1,12 @@
-file = open('alumnos.txt','r')
-nombres = file.readlines()
-print(nombres)
-file.close()
+#file = open('alumnos.txt', 'r')
+#nombres = file.readlines()
+#print(nombres)
+#file.close()
 
-for item in nombres:
-    print(item,end = '')
+#file = open('alumnos2.txt', 'w')
+#file.write('NUEVO HOLA MUNDO!!!')
+#file.close()
+
+file = open('alumnos2.txt', 'a')
+file.write('\n' + 'OTRO HOLA MUNDO!!!')
+file.close()
